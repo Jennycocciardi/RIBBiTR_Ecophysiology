4885404d35d5e4be37169397ca96e7b599b0f3a9
Jenny Cocciardi <50609158+Jennycocciardi@users.noreply.github.com>
2023-04-21 09:46:03 -0500

    Update TrimHOBOFiles.R

--- TrimHOBOFiles.R
+++ TrimHOBOFiles.R
@@ -47,7 +47,7 @@ library(lubridate)
 # To trim HOBO data based on when the logger was deployed and collected from the field,
 # first create a single .csv file with the name of the each file, the time it was deployed,
 # and the time is was removed from the field. It should be in the format:
-  # 'file_name, date.time.start, date.time.stop'
+  # 'file_name, start_date.time, stop_date.time'
 
 trimming_info <- read.csv("trimming_info.csv")
 
@@ -56,12 +56,12 @@ trimming_info <- read.csv("trimming_info.csv")
 # For example, Sierra Nevada date.time of start and stop for loggers is currently
 # formatted as: mdy hms. To change this:
 # Convert the character vector to POSIXct format
-trimming_info$date.time_start <- as.POSIXct(trimming_info$date.time_start, 
+trimming_info$start_date.time <- as.POSIXct(trimming_info$start_date.time, 
                                             format = "%m/%d/%y %H:%M", tz="UTC")
-trimming_info$date.time_stop <- as.POSIXct(trimming_info$date.time_stop, 
+trimming_info$stop_date.time <- as.POSIXct(trimming_info$stop_date.time, 
                                             format = "%m/%d/%y %H:%M", tz="UTC")
-trimming_info$date.time_start <- ymd_hms(trimming_info$date.time_start)
-trimming_info$date.time_stop <- ymd_hms(trimming_info$date.time_stop)
+trimming_info$start_date.time <- ymd_hms(trimming_info$start_date.time)
+trimming_info$stop_date.time <- ymd_hms(trimming_info$stop_date.time)
 
 ## Set output directory for new files - this insures that we are not overwriting the originals
 # out_dir <- "/path/to/new/folder/for/output" --> if want to put files into new directory
@@ -83,8 +83,8 @@ for (file_name in file_list) {
   trimming <- trimming_info[trimming_info$file_name == file_name, ]
   
   # Filter the data based on the trimming start and end times
-  data.trimmed <- data[data$date.time >= trimming$date.time_start &
-                         data$date.time <= trimming$date.time_stop, ]
+  data.trimmed <- data[data$date.time >= trimming$start_date.time &
+                         data$date.time <= trimming$stop_date.time, ]
   
   # Write the trimmed data to a new file
   write_xlsx(data.trimmed, paste0("trimmed_", file_name))
