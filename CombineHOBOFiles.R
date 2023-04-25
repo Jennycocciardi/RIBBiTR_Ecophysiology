34ee6d0947477b1560d0ef3d7626abe691528d0e
Jennycocciardi <jenny.cocciardi@gmail.com>
2023-04-24 19:59:34 -0500

    updates

--- CombineHOBOFiles.R
+++ CombineHOBOFiles.R
@@ -10,20 +10,21 @@ library(readxl)
 library(writexl)
 library(dplyr)
 
-# Set working directory to folder containing the Excel files
+# Set working directory to folder containing the Excel files - this should be a new 
+# directory called 'output' as specified in the trimmining script.
 setwd("/path/to/folder/containing/excel/files")
 
 # Set output directory for new files - this insures that we are not overwriting the originals
 out_dir <- "/path/to/new/folder/for/output"
 
 # Extract the site, deployment location, and height/depth from each file and create new columns.
-# List all Excel files in the folder
-file_list <- list.files(pattern = "*.xlsx")
+# List all .csv files in the folder
+file_list <- list.files(pattern = "*.csv")
 
 # Loop over files and extract the desired part of the file name to make new column
 for (file_name in file_list) {
   # Read the Excel file
-  data <- read_excel(file_name)
+  data <- read.csv(file_name)
 
   # Extract site from file name *note, this will only work if files names are 
   # standardized in the format: 'Site_DeploymentLocation_heightORdepth'
@@ -43,23 +44,20 @@ for (file_name in file_list) {
   data$height <- height
   # Adding in a column for hobo logger/file name to conserve as a link to metadata and original files
   # This needs to be different from file_name, which is a part of a the function for file_list
-  data$hobo_name <- file_name
+  # Get the file name without extension
+  file_name_without_ext <- file_path_sans_ext(basename(file_name))
+  data$hobo_name <- file_name_without_ext
   
   # Save the modified data to a new Excel file with a modified file name, and in a new directory
-  write_xlsx(data, paste0(out_dir, "new_", file_name))
+  write.csv(data, paste0("new_", file_name))
 }
 
 
 # Combine files by deployment location so that we have one file per location (e.g., sun, shade, 
 # water, etc.)
 
-# Set working directory to new folder
-setwd("/path/to/new/folder")
-# List all Excel files in the folder. Will need to do this again since we have a new folder.
-file_list <- list.files(pattern = "*.xlsx")
-
-# Create a list of data frames, with each data frame corresponding to one Excel file
-dfs <- lapply(file_list, read_excel)
+# List all files in the folder. Will need to do this again since we have a new folder.
+file_list <- list.files(pattern = "new_*")
 
 # Combine the data frames into one data frame, adding the grouping variable
 combined_df <- bind_rows(dfs, .id = "file_name") %>%
@@ -69,6 +67,6 @@ combined_df <- bind_rows(dfs, .id = "file_name") %>%
 split_dfs <- split(combined_df, f = combined_df$location)
 
 # Write each data frame to a separate file based on the grouping variable
-walk(split_dfs, ~ write_csv(.x, paste0(.x$location[1], ".csv")))
+walk(split_dfs, ~ write.csv(.x, paste0(.x$location[1], ".csv")))
 
 
--- TrimHOBOFiles.R
+++ TrimHOBOFiles.R
@@ -3,37 +3,38 @@
 # email: jmcoccia@olemiss.edu
 # Date: 2023-April-19
 
-# Before starting, make sure that all files are standardized to the same timezone and that
-# the 'datetime' column is named the same.
-  # This can be automated at some point, but I'm still figuring out how to automate this across 
-  # all files within one folder where date-time column name differs.
-library(readxl)
-library(openxlsx)
+# Before starting, make sure that all files are standardized to the same timezone of where
+# the loggers were deployed.
+
+library(dplyr)
 
 # Set the directory path
 directory <- "/path/to/folder/containing/hobo/data"
 
 # Loop over files in the directory
-for (filename in list.files(directory, pattern = "\\.xlsx$")) {
-  # Read the .csv file (if files are exported as .xlsx, parts of this code will have to be modified to reflect that.
-  data <- read_excel(file.path(directory, filename))
+for (filename in list.files(directory, pattern = "\\.csv$")) {
+  # Read the .csv file (if files are exported as .xlsx, parts of this code will have to be modified to reflect that)
+  data <- read.csv(file.path(directory, filename))
   
-  # Rename the column
-  colnames(data)[which(colnames(data) == "Date-Time (PDT)")] <- "date.time"
+  # Rename the column, this will change based on timezone deployed
+  colnames(data)[which(colnames(data) == "Date.Time..GMT.05.00")] <- "date.time.GMT.minus5"
+  colnames(data)[which(colnames(data) == "Temp...C")] <- "temp.C"
+  colnames(data)[which(colnames(data) == "RH...")] <- "RH"
+  colnames(data)[which(colnames(data) == "DewPoint...C")] <- "dewpt.C"
   
   # Write the updated xlsx file
-  write.xlsx(data, file.path(directory, filename), row.names = FALSE)
+  write.csv(data, file.path(directory, filename), row.names = FALSE)
 }
 
 # Check to make sure all files have new 'date.time' column
-file_list <- list.files(pattern = "*.xlsx")
+file_list <- list.files(pattern = "*.csv")
 
 for (file in file_list) {
   # read in the data
-  data <- read_excel(file)
+  data <- read.csv(file)
   
   # check if "date.time" is in the column names
-  if ("date.time" %in% colnames(data)) {
+  if ("date.time.GMT.minus2" %in% colnames(data)) {
     print(paste0("File ", file, " has the 'date.time' column."))
   } else {
     print(paste0("File ", file, " does not have the 'date.time' column."))
@@ -41,8 +42,9 @@ for (file in file_list) {
 }
 
 library(tidyverse)
-library(readxl)
 library(lubridate)
+library(tools)
+library(xts)
 
 # To trim HOBO data based on when the logger was deployed and collected from the field,
 # first create a single .csv file with the name of the each file, the time it was deployed,
@@ -51,44 +53,65 @@ library(lubridate)
 
 trimming_info <- read.csv("trimming_info.csv")
 
-# Make sure date and time are in a common format. This may depend on how it was inputed
-# by the field team so may differ between study areas.
-# For example, Sierra Nevada date.time of start and stop for loggers is currently
-# formatted as: mdy hms. To change this:
-# Convert the character vector to POSIXct format
-trimming_info$start_date.time <- as.POSIXct(trimming_info$start_date.time, 
-                                            format = "%m/%d/%y %H:%M", tz="UTC")
-trimming_info$stop_date.time <- as.POSIXct(trimming_info$stop_date.time, 
-                                            format = "%m/%d/%y %H:%M", tz="UTC")
-trimming_info$start_date.time <- ymd_hms(trimming_info$start_date.time)
-trimming_info$stop_date.time <- ymd_hms(trimming_info$stop_date.time)
+# Make sure date and time are in a common format. This will depend on how data is exported
+# from hoboWare and how the start and data time for deployement are inputted in the 
+#'hobo deployement logger info' spreadsheet.
+# For example, if date.time of start and stop for loggers is currently formatted to 
+# mdy hms, we will need to change to YMD HMS. To change this: onvert the character 
+# vector to POSIXct format (may need to try these formats depending on how they were inputted)
+# into excel...tryFormats = c("%Y-%m-%d %H:%M:%OS","%Y/%m/%d %H:%M:%OS", "%Y-%m-%d %H:%M",
+#                                "%Y/%m/%d %H:%M", "%Y-%m-%d", "%Y/%m/%d")
 
-## Set output directory for new files - this insures that we are not overwriting the originals
-# out_dir <- "/path/to/new/folder/for/output" --> if want to put files into new directory
+trimming_info$start_date.time <- as.POSIXct(trimming_info$start_date.time, 
+                                            format = "%m/%d/%y %H:%M")
+# This displays timezone as UTC, so change this based on timezone of data collection.
+# Use the below function to do this, along with codes for the timezone as found in :
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones 
+trimming_info$start_date.time <- force_tz(trimming_info$start_date.time, 
+                                          tzone = "Brazil/DeNoronha")
+trimming_info$stop_date.time <- as.POSIXct(trimming_info$stop_date.time, 
+                                            format = "%Y-%m-%d %H:%M")
+trimming_info$stop_date.time <- force_tz(trimming_info$stop_date.time, 
+                                         tzone = "Brazil/DeNoronha")
+
+
+## Create and set output directory for new files - this insures that we are not overwriting the originals
+ dir.create("output")
+ # out_dir <- "/path/to/new/folder/for/output" --> if want to put files into new directory
 
 # Loop over each file in the directory
-file_list <- list.files(pattern = "*.xlsx")
+file_list <- list.files(pattern = "*.csv")
 
 for (file_name in file_list) {
-  if (endsWith(file_name, ".xlsx") & file_name != "trimming_info.csv") {
+  if (endsWith(file_name, ".csv") & file_name != "trimming_info.csv"
+    & file_name != "BO01_sun_50cm.csv") {
+    
+    # Get the file name without extension
+    file_name_without_ext <- file_path_sans_ext(basename(file_name))
     # Check if the file is included in the trimming information file
-    if (file_name %in% trimming_info$file_name) {
+    if (file_name_without_ext %in% trimming_info$hobo_name) {
       # Read the data into R
-      data <- read_excel(file_name)
+      data <- read.csv(file_name)
   
-  # Convert to POSIXct format
-  data$date.time <- ymd_hms(data$date.time, tz = "UTC")
+  # Convert to POSIXct format (the name of data.time colomn in data files will differ
+      # depending on the timezone and so will need to adjust to fit data)
+  data$date.time.GMT.minus2 <- as.POSIXct(data$date.time.GMT.minus2, 
+                                             format = "%y/%m/%d %H:%M:%S")
+  data$date.time.GMT.minus2 <- force_tz(data$date.time.GMT.minus2, 
+                                           tzone = "Brazil/DeNoronha")
   
   # Get the trimming information for this file
-  trimming <- trimming_info[trimming_info$file_name == file_name, ]
+  trimming <- trimming_info[trimming_info$hobo_name == file_name_without_ext, ]
   
   # Filter the data based on the trimming start and end times
-  data.trimmed <- data[data$date.time >= trimming$start_date.time &
-                         data$date.time <= trimming$stop_date.time, ]
+  # data.trimmed <- data[data$date.time.GMT.minus5 >= trimming$start_date.time &
+  #                       data$date.time.GMT.minus5 <= trimming$stop_date.time, ]
+  
+  # If only one side needs trimming
+  data.trimmed <- data[data$date.time.GMT.minus2 >= trimming$start_date.time, ]
   
   # Write the trimmed data to a new file
-  write_xlsx(data.trimmed, paste0("trimmed_", file_name))
+  write.csv(data.trimmed, file.path(out_dir, paste0(file_name_without_ext,"_trimmed.csv")))
     }
   }
 }
-
