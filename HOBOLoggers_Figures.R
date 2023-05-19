b1205d300d1a25beb3318f386170f90ddd4700b1
Jenny Cocciardi <50609158+Jennycocciardi@users.noreply.github.com>
2023-05-19 09:29:18 -0500

    Update HOBOLoggers_Figures.R

--- HOBOLoggers_Figures.R
+++ HOBOLoggers_Figures.R
@@ -9,24 +9,25 @@
 # Load necessary libraries
 library(ggplot2)
 library(tidyr)
+library(lubricate)
 
 # set working directory and load file
 data <- read.csv('sun.csv')
 
-# Make sure date.time column is recognized as.date
+# Make sure date.time column is recognized as date
 # check structure
 str(data) #make sure variables are recognized as they should be
 # data$site <- as.character(data$site)
 # data$date.time <- as.POSIXct(data$date.time, format = "%Y-%m-%d %H:%M:%S")
 data$date.time <- as.POSIXct(data$date.time, format = "%m/%d/%y %H:%M")
-data$date.time <- force_tz(data$date.time, tzone = "America/Los_Angeles") 
-  # "Etc/GMT+4", "Brazil/DeNoronha", "America/Los_Angeles", "America/Panama"
+data$date.time <- force_tz(data$date.time, tzone = "America/Los_Angeles") # change timezone to relevant study system
+  # options used for RIBBiTR study sites: "Etc/GMT+4", "Etc/GMT+5" "Brazil/DeNoronha", "America/Los_Angeles", "America/Panama"
 
 # Line plot by site and deployment location
 ggplot(data, aes(x = date.time, y = temp.C, group = interaction(site, height), color = height)) +
   geom_line() +
   facet_grid(rows = vars(site)) +
-  labs(x = "Date", y = "Temperature (C)", color = "Height/depth", title = "Brazil: sun") +
+  labs(x = "Date", y = "Temperature (C)", color = "Height/depth", title = "Brazil: sun") + # change labels/title based on data used
   scale_color_brewer(palette = "YlOrBr") +
   scale_x_datetime(date_labels =  "%b-%d-\n%Y", 
                    date_breaks = "2 weeks", 
@@ -46,18 +47,19 @@ ggplot(data = subset(data, grepl("\\b50cm\\b", height)), # when need to subset d
 # ggplot(data, 
   aes(x = date.time, y = temp.C, group = interaction(site, height), color = site, linetype = height)) +
   geom_line() +
-  labs(x = "", y = "Temperature (C)", color = "Sites", title = "Sierra Nevada, sun (50cm)") +
+  labs(x = "", y = "Temperature (C)", color = "Sites", title = "Sierra Nevada, sun (50cm)") + # change labels/title based on data used
   guides(color = guide_legend(ncol=2),
          linetype = "none") +
   scale_x_datetime(date_labels =  "%b-%d-%y", 
                    breaks = seq(min(data$date.time), max(data$date.time), by ="2 weeks"),
                    expand = c(0, 0)) +
   scale_y_continuous(breaks=seq(-10,55,5),limits = c(-10,55)) +
-  # scale_color_brewer(palette = "Greens", direction=-1) + # for shade
-  # scale_color_manual(values = c("#663300","#993300", "#CC6600", "#CC9900", "#FFCC66" )) + #for soil, 
-  # scale_color_brewer(palette = "Blues", direction=-1) + # for water
-  scale_color_brewer(palette = "YlOrRd", direction=-1) + # for sun
-  # scale_color_manual(values = colorRampPalette(brewer.pal(9, "Blues"))(11)) + #for water, if more variables then allowed for pallet
+  # use the following colors/palettes based on the deployement location
+    # scale_color_brewer(palette = "Greens", direction=-1) + # for shade
+    # scale_color_manual(values = c("#663300","#993300", "#CC6600", "#CC9900", "#FFCC66" )) + #for soil, 
+    # scale_color_brewer(palette = "Blues", direction=-1) + # for water
+    # scale_color_brewer(palette = "YlOrRd", direction=-1) + # for sun
+    # scale_color_manual(values = colorRampPalette(brewer.pal(9, "Blues"))(11)) + #for water, if more variables then allowed for pallet
   theme_bw() +
   theme(panel.grid = element_blank(),
         axis.title = element_text(size=9),
@@ -71,7 +73,7 @@ ggplot(data = subset(data, grepl("\\b50cm\\b", height)), # when need to subset d
 
 # Use the 'here' package to keep figures organized in github directory
 # library(here)
-ggsave(here("/Users/jennycocciardi/Desktop", "SN_sun_50cmB.png"), 
+ggsave(here("folder/to/place/figures", "SN_sun_50cmB.png"), # change figure name to reflect data
        width=5, height=3, dpi=300) #setting plot width to reflect timeline of data
 
 
@@ -80,22 +82,21 @@ ggsave(here("/Users/jennycocciardi/Desktop", "SN_sun_50cmB.png"),
 # set working directory and load file
 data <- read.csv('combined.csv')
 
-# Make sure date.time column is recognized as.date
+# Make sure date.time column is recognized as date
 # check structure
 str(data) #make sure variables are recognized as they should be
 # data$site <- as.character(data$site)
 data$date.time <- as.POSIXct(data$date.time, format = "%Y-%m-%d %H:%M:%S")
 # data$date.time <- as.POSIXct(data$date.time, format = "%m/%d/%y %H:%M")
-data$date.time <- force_tz(data$date.time, tzone = "Etc/GMT+4") 
-# "Etc/GMT+4", "Brazil/DeNoronha", "America/Los_Angeles", "America/Panama"
+data$date.time <- force_tz(data$date.time, tzone = "Etc/GMT+4") # change timezone to relevant study system
+  # options used for RIBBiTR study sites: "Etc/GMT+4", "Etc/GMT+5" "Brazil/DeNoronha", "America/Los_Angeles", "America/Panama"
 
 # plot for combined and smoothed data
-ggplot(data = subset(data, grepl("RV", site) & grepl("\\b0cm\\b|\\b5cm\\b|\\b-5cm\\b", height)), # when need to subset one site data
+ggplot(data = subset(data, grepl("RV", site) & grepl("\\b0cm\\b|\\b5cm\\b|\\b-5cm\\b", height)), # when need to subset one site data (for example, only want to see 5cm soil)
        # ggplot(data, 
        aes(x = date.time, y = temp.C, group = interaction(location, height), color = location)) +
   geom_smooth(se = FALSE) +
-  labs(x = "", y = "Temperature (C)", color = "Location", title = "Microhabitat temperature data
-Pennsylvania (RV)") +
+  labs(x = "", y = "Temperature (C)", color = "Location", title = "Microhabitat temperature data Pennsylvania (RV)") +
   scale_x_datetime(date_labels =  "%b-%d-%y", 
                    breaks = seq(min(data$date.time), max(data$date.time), by ="2 weeks"),
                    expand = c(0, 0)) +
@@ -113,21 +114,21 @@ Pennsylvania (RV)") +
         legend.justification = c("right", "top"),
         legend.key.size = unit(0.5, 'lines'))
 
-ggsave(here("/Users/jennycocciardi/GitHub/RIBBiTR_Ecophysiology/Figures", "PA_RV_combined.png"), 
+ggsave(here("/folder/to/save/figures", "PA_RV_combined.png"), # change figure name to reflect data
        width=4, height=4, dpi=300) #setting plot width to reflect timeline of data
 
 # plot for smoothed data seperated by height at the same site
-ggplot(data = subset(data, grepl("RV", site) & grepl("soil", location)), # when need to subset one site data
+ggplot(data = subset(data, grepl("site ID", site) & grepl("deployement location", location)), # when need to subset one site data
        # ggplot(data, 
        aes(x = date.time, y = temp.C, group = height, color = height)) +
   geom_smooth(se = FALSE) +
-  labs(x = "", y = "Temperature (C)", color = "Height", title = "Soil: Pennsylvania (RV)") +
+  labs(x = "", y = "Temperature (C)", color = "Height", title = "Soil: Pennsylvania (RV)") + # change title based on data used
   scale_x_datetime(date_labels =  "%b-%d-%y", 
                    breaks = seq(min(data$date.time), max(data$date.time), by ="2 weeks"),
                    expand = c(0, 0)) +
   scale_y_continuous(breaks=seq(2.5,22.5,2.5),limits = c(2.5,22.5)) +
   scale_color_manual(breaks = c("-5cm","-10cm"),
-                     values = c("#993300", "#CC6600")) + 
+                     values = c("#993300", "#CC6600")) + # change colors based on deployement location (water, sun, etc)
   theme_bw() +
   theme(panel.grid = element_blank(),
         axis.title = element_text(size=8),
@@ -139,5 +140,5 @@ ggplot(data = subset(data, grepl("RV", site) & grepl("soil", location)), # when 
         legend.justification = c("right", "top"),
         legend.key.size = unit(0.5, 'lines'))
 
-ggsave(here("/Users/jennycocciardi/GitHub/RIBBiTR_Ecophysiology/Figures", "PA_RV_soil.png"), 
+ggsave(here("/folder/to/save/figures", "PA_RV_soil.png"), # change figure name to reflect data
        width=3, height=3, dpi=300) #setting plot width to reflect timeline of data
