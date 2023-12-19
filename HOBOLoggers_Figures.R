a3a82648e07b7453ff98b1dfaa1300b4da4d748a
Jennycocciardi <jenny.cocciardi@gmail.com>
2023-12-19 11:24:27 -0600

    Update HOBOLoggers_Figures.R

--- HOBOLoggers_Figures.R
+++ HOBOLoggers_Figures.R
@@ -11,8 +11,78 @@ library(ggplot2)
 library(tidyr)
 library(lubricate)
 
+
+# Check trimmed and combined data to make sure it's ready to go into database
+
 # set working directory and load file
-data <- read.csv('sun.csv')
+data <- read.csv('combined.csv')
+
+# Make sure DateTime column is recognized as date
+# check structure
+str(data) #make sure variables are recognized as they should be
+# data$Site <- as.character(data$Site)
+data$DateTime <- as.POSIXct(data$DateTime, format = "%Y-%m-%d %H:%M:%S")
+# data$DateTime <- as.POSIXct(data$DateTime, format = "%m/%d/%y %H:%M")
+data$DateTime <- force_tz(data$DateTime, tzone = "Etc/GMT+4") # change timezone to relevant study system
+# options used for RIBBiTR study sites: "Etc/GMT+4", "Etc/GMT+5" "Brazil/DeNoronha", "America/Los_Angeles", "America/Panama"
+
+# Line plot by site and deployment location
+ggplot(data, aes(x = DateTime, y = Temperature, group = interaction(Site, Height), 
+                 color = Site)) +
+  geom_line() +
+  facet_grid(rows = vars(Site)) +
+  labs(x = "Date", y = "Temperature (C)", color = "Height/depth") +
+  scale_color_brewer(palette = "YlOrBr") +
+  scale_x_datetime(date_labels =  "%b-%d-\n%Y", 
+                   date_breaks = "2 weeks", 
+                   minor_breaks = NULL) +
+  scale_y_continuous(breaks=seq(10,30,5)) +
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        panel.background = element_rect(fill = "white"),
+        strip.background = element_rect(fill = "linen"))
+
+
+
+
+# More plots below for looking at data
+library(RColorBrewer)
+display.brewer.all(colorblindFriendly = TRUE)
+library(gridExtra)
+
+# Line plot by deployment location
+ggplot(data = subset(data, grepl("\\b50cm\\b", height)), # when need to subset data based on height/depth
+       # ggplot(data, 
+       aes(x = date.time, y = temp.C, group = interaction(site, height), color = site, linetype = height)) +
+  geom_line() +
+  labs(x = "", y = "Temperature (C)", color = "Sites", title = "Sierra Nevada, sun (50cm)") + # change labels/title based on data used
+  guides(color = guide_legend(ncol=2),
+         linetype = "none") +
+  scale_x_datetime(date_labels =  "%b-%d-%y", 
+                   breaks = seq(min(data$date.time), max(data$date.time), by ="2 weeks"),
+                   expand = c(0, 0)) +
+  scale_y_continuous(breaks=seq(-10,55,5),limits = c(-10,55)) +
+  # use the following colors/palettes based on the deployement location
+  # scale_color_brewer(palette = "Greens", direction=-1) + # for shade
+  # scale_color_manual(values = c("#663300","#993300", "#CC6600", "#CC9900", "#FFCC66" )) + #for soil, 
+  # scale_color_brewer(palette = "Blues", direction=-1) + # for water
+  # scale_color_brewer(palette = "YlOrRd", direction=-1) + # for sun
+  # scale_color_manual(values = colorRampPalette(brewer.pal(9, "Blues"))(11)) + #for water, if more variables then allowed for pallet
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        axis.title = element_text(size=9),
+        axis.text.x = element_text(size=8),
+        plot.title = element_text(size=11, face="bold"),
+        legend.text = element_text(size=8),
+        legend.title = element_text(size=9),
+        legend.position = c(0.99, 0.99),
+        legend.justification = c("right", "top"),
+        legend.key.size = unit(0.5, 'lines'))
+
+# Use the 'here' package to keep figures organized in github directory
+# library(here)
+ggsave(here("folder/to/place/figures", "SN_sun_50cmB.png"), # change figure name to reflect data
+       width=5, height=3, dpi=300) #setting plot width to reflect timeline of data
 
 # Make sure date.time column is recognized as date
 # check structure
@@ -22,6 +92,8 @@ str(data) #make sure variables are recognized as they should be
 data$date.time <- as.POSIXct(data$date.time, format = "%m/%d/%y %H:%M")
 data$date.time <- force_tz(data$date.time, tzone = "America/Los_Angeles") # change timezone to relevant study system
   # options used for RIBBiTR study sites: "Etc/GMT+4", "Etc/GMT+5" "Brazil/DeNoronha", "America/Los_Angeles", "America/Panama"
+
+
 
 # Line plot by site and deployment location
 ggplot(data, aes(x = date.time, y = temp.C, group = interaction(site, height), color = height)) +
