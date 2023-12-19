6bb7a7bb47eacc2b8466b423bc6977b7846d4e96
Jennycocciardi <jenny.cocciardi@gmail.com>
2023-12-19 11:18:02 -0600

    Update CombineHOBOFiles.R

--- CombineHOBOFiles.R
+++ CombineHOBOFiles.R
@@ -14,9 +14,11 @@ library(dplyr)
 # directory called 'output' as specified in the trimming script.
 setwd("/path/to/folder/containing/files")
 
+
 # Extract the site, deployment location, and height/depth from each file and create new columns.
 # List all .csv files in the folder
 file_list <- list.files(pattern = "*.csv")
+
 
 # Loop over files and extract the desired part of the file name to make new column
 for (file_name in file_list) {
@@ -51,8 +53,8 @@ for (file_name in file_list) {
 }
 
 
-# Combine files by deployment location so that we have one file per location (e.g., sun, shade, 
-# water, etc.)
+
+# Combine files
 
 # List all files in the folder. Will need to do this again since we have newly created files.
 file_list <- list.files(pattern = "new_*")
@@ -65,12 +67,38 @@ combined_df <- bind_rows(lapply(file_list, read.csv)) %>%
   # combined_df <- bind_rows(lapply(file_list, function(file) read.csv(file, colClasses = "character"))) %>%
   # select(hobo_name, site, location, height, everything())
 
+
+
+# Prepare the combined.csv file for input into RIBBiTR database
+    # combined_df should have these columns in this order: 
+    # StudyArea, hobo_name, Site, Location, Height, DateTime, Temperature, TimeZone, Intensity.Lux, RH, DewPoint
+    # Select these columns to keep:
+columns_to_keep <- c("StudyArea","hobo_name", "Site", "Location", "Height", "DateTime", 
+                     "Temperature", "TimeZone", "Intensity.Lux", "RH", "DewPoint")
+    
+    # Rename columns and get rid of extra columns, add in StudyArea
+combined_df_database_ready <- combined_df %>%
+  rename(
+    Site = site,
+    Location = location,
+    Height = height,
+    DateTime = date.time,
+    Temperature = temperature,
+    TimeZone = timezone,
+    Intensity.Lux = intensity.lux,
+    RH = rh,
+    DewPoint = DewPt...C)  %>%
+  mutate(StudyArea = 'PA') %>% #add a new column 'StudyArea' to indicate which study system the data 
+                           #is for - either PA, Brazil, Panama, SierraNevada 
+  select(columns_to_keep) #select the columns to keep
+
 # Write one overall dataframe
-write.csv(combined_df, file = "combined.csv")
+write.csv(combined_df_database_ready, file = "combined.csv", row.names = FALSE)
 
 # Split the combined data frame into separate data frames based on the grouping variable
 split_dfs <- split(combined_df, f = combined_df$location)
 
 # Write each data frame to a separate file based on the grouping variable
 walk(split_dfs, ~ write.csv(.x, paste0(.x$location[1], ".csv")))
+
 
